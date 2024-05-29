94bfe2ef052af1d052bf76c0c987bffbca2ad0e0
iamziggi <oluwaseunakinola4@gmail.com>
2024-05-29 14:16:52 +0100

    new update

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ function togglePassword() {
 
   if (passwordInput.type === 'password') {
     passwordInput.type = 'text';
-    eyeIcon.classList.remove('fa-eye');
-    eyeIcon.classList.add('fa-eye-slash');
-  } else {
-    passwordInput.type = 'password';
     eyeIcon.classList.remove('fa-eye-slash');
     eyeIcon.classList.add('fa-eye');
+  } else {
+    passwordInput.type = 'password';
+    eyeIcon.classList.remove('fa-eye');
+    eyeIcon.classList.add('fa-eye-slash');
   }
 }
 
